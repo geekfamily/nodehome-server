ea39a6e99afa3f2d8fb11d90ad6e52a49bb5a8a8
geekfamily <jdmotley@gmail.com>
2016-04-10 14:14:13 -0700

    fixed url ref

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
-{
-  "name": "server",
-  "version": "0.0.0",
-  "description": "node home server",
-  "main": "server.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "start": "node server.js"
-  },
-  "keywords": [
-    "node",
-    "iot"
-  ],
-  "author": "jmotley",
-  "license": "ISC",
-  "dependencies": {
-    "mosca": "^1.1.2"
+var mosca = require('mosca')
+
+var ascoltatore = {
+  //using ascoltatore
+  type: 'mongo',        
+  url: 'mongodb://192.168.1.157:27017/mqtt',
+  pubsubCollection: 'ascoltatori',
+  mongo: {}
+};
+
+var moscaSettings = {
+  port: 1883,
+  backend: ascoltatore,
+  persistence: {
+    factory: mosca.persistence.Mongo,
+    url: 'mongodb://192.168.1.157:27017/mqtt'
   }
+};
+
+var server = new mosca.Server(moscaSettings);
+server.on('ready', setup);
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);     
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.payload);
+});
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running')
 }
